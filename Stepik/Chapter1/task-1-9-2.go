2c17224babfdb06667bc25d8608c1f0ea15c7532
Eddie55d <89diman@mail.ru>
2023-04-26 13:23:01 +0300

    add tasks 1-9

---
+++ Stepik/Chapter1/task-1-9-2.go
@@ -0,0 +1,31 @@
+// По данному трехзначному числу определите, все ли его цифры различны.
+
+// Формат входных данных
+// На вход подается одно натуральное трехзначное число.
+
+// Формат выходных данных
+// Выведите "YES", если все цифры числа различны, в противном случае - "NO".
+
+// Sample Input 1: 237
+// Sample Output 1: YES
+
+// Sample Input 2: 117
+// Sample Output 2: NO
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+	str := strconv.Itoa(a)
+	if str[0] != str[1] && str[1] != str[2] && str[0] != str[2] {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
