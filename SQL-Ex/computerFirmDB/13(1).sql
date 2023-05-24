543288707cb45bda452a53f7258b9eeb91976690
Eddie55d <89diman@mail.ru>
2023-05-24 21:46:44 +0300

    removed unnecessary files

--- SQL-Ex/computerFirmDB/13(1).sql
+++
@@ -1,9 +0,0 @@
--- Найдите среднюю скорость ПК, выпущенных производителем A.
--- Справка по теме:
---  Получение итоговых значений
---  Явные операции соединения
-
-SELECT AVG(speed)
-FROM PC
-RIGHT JOIN Product ON Product.model = PC.model
-WHERE maker='A'
--- SQL-Ex/computerFirmDB/9(1).sql
+++
@@ -1,9 +0,0 @@
--- Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker
--- Справка по теме:
---  Явные операции соединения
---  Простой оператор SELECT
-
-SELECT DISTINCT maker
-FROM Product
-JOIN PC ON PC.model = Product.model
-WHERE speed >= 450
